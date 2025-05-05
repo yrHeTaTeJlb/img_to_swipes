ff21963dd35a67a04ac1a80727e992d087123117
yrHeTaTeJlb <>
2025-05-05 02:18:21 +0200

    Refactor Swiper class to use private _actions attribute for ActionChains

--- platforms/ios/swiper.py
+++ platforms/ios/swiper.py
@@ -12,18 +12,18 @@ class Swiper:
     def __init__(self) -> None:
         driver = webdriver.Remote(options=XCUITestOptions())
 
-        self.actions = ActionChains(driver, duration=15)
+        self._actions = ActionChains(driver, duration=15)
         touch_input = PointerInput(interaction.POINTER_TOUCH, 'touch')
-        self.actions.w3c_actions = ActionBuilder(driver, mouse=touch_input, duration=15)
+        self._actions.w3c_actions = ActionBuilder(driver, mouse=touch_input, duration=15)
 
     def swipe(self, points: Iterable[tuple[int, int]]) -> None:
         start = next(iter(points), None)
         if start is None:
             return
 
-        self.actions.w3c_actions.pointer_action.move_to_location(start[0], start[1])
-        self.actions.w3c_actions.pointer_action.pointer_down()
+        self._actions.w3c_actions.pointer_action.move_to_location(start[0], start[1])
+        self._actions.w3c_actions.pointer_action.pointer_down()
         for x, y in points:
-            self.actions.w3c_actions.pointer_action.move_to_location(x, y)
-        self.actions.w3c_actions.pointer_action.release()
-        self.actions.perform()
+            self._actions.w3c_actions.pointer_action.move_to_location(x, y)
+        self._actions.w3c_actions.pointer_action.release()
+        self._actions.perform()
