d2a98eae94a116f0b8eca0ef0c59c311215286d6
yrHeTaTeJlb <>
2025-08-21 18:14:56 +0200

    Fixed npm-cli.js path for linux

--- platform_tools/linux/nodejs/npm.sh
+++ platform_tools/linux/nodejs/npm.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 NODE_PATH="$(node -p 'require("path").dirname(process.execPath)')"
-node "$NODE_PATH/node_modules/npm/bin/npm-cli.js" "$@"
+node "$NODE_PATH/../lib/node_modules/npm/bin/npm-cli.js" "$@"
